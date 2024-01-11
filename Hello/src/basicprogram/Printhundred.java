f26a2c060d87d3e91e06743115417789107df7f7
121 <121@DESKTOP-JQ4STD6>
2024-01-11 21:10:42 +1300

    Initial Commit.

---
+++ Hello/src/basicprogram/Printhundred.java
@@ -0,0 +1,16 @@
+package basicprogram;
+
+public class Printhundred {
+
+	public static void main(String[] args) {
+	int a=1;
+	while(a<10)
+	{
+		System.out.println("hello world");
+		a++;
+	
+	}
+
+	}
+
+}
